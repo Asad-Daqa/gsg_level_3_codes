d7a7061b222b723b858a23bd8bdfa41294d8baf5
Asad Daqa <asad.daqa.2006@gmail.com>
2025-08-12 15:10:20 +0300

    Update solution.cpp

--- activity_schedules/solution.cpp
+++ activity_schedules/solution.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int cnt=0, n;
+vector<string> a ={"Swimming", "Running", "Football"};
 
+void rec(vector<string>v,int idx=0)
+{
+    if(idx==n){if(v.size()==n){for(auto it : v)cout<<it<<" ";cout<<endl;cnt++;}return;}
+    for (int i = 0; i < 3; i++) {
+        vector<string>v1=v;
+        if(!v1.empty()&& v1.back()==a[i])continue ;
+        v1.push_back(a[i]);
+        rec(v1,idx+1);
+    }
+
+}
+int main()
+{
+
+    cin>>n;
+    rec({});
+    // formuila :count always be 3*(2^n-1);
+    cout<<"Count : "<<cnt<<endl;
+}
